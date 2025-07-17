671432e22f2f410dd6c7f241bf19ca85e60163b4
Chris <ctang@zetaglobal.com>
2025-07-17 16:09:10 -0400

    formatting

--- packages/frontend-react/src/App.jsx
+++ packages/frontend-react/src/App.jsx
@@ -4,9 +4,12 @@ function App() {
   return (
     <div className="App">
         <h1>Disqus SSO Demo - React</h1>
-        <a href="https://github.com/disqus/sso-demo/tree/main/packages/frontend-react" target="_blank" rel="noopener noreferrer">View Frontend Source Code on GitHub</a>
-
-        <a href="https://github.com/disqus/sso-demo/tree/main/packages/backend" target="_blank" rel="noopener noreferrer">View Backend Source Code on GitHub</a>
+        <div>
+            <a href="https://github.com/disqus/sso-demo/tree/main/packages/frontend-react" target="_blank" rel="noopener noreferrer">View Frontend Source Code on GitHub</a>
+        </div>
+        <div>
+            <a href="https://github.com/disqus/sso-demo/tree/main/packages/backend" target="_blank" rel="noopener noreferrer">View Backend Source Code on GitHub</a>
+        </div>
         <DisqusSSO />
     </div>
   );

be95ece4a14aba89c86dd5f6def540d36142c5a7
Chris <ctang@zetaglobal.com>
2025-07-17 15:41:40 -0400

    add links to each demo

--- packages/frontend-react/src/App.jsx
+++ packages/frontend-react/src/App.jsx
@@ -3,8 +3,11 @@ import DisqusSSO from './components/DisqusSSO';
 function App() {
   return (
     <div className="App">
-      <h1>Disqus SSO Demo - React</h1>
-      <DisqusSSO />
+        <h1>Disqus SSO Demo - React</h1>
+        <a href="https://github.com/disqus/sso-demo/tree/main/packages/frontend-react" target="_blank" rel="noopener noreferrer">View Frontend Source Code on GitHub</a>
+
+        <a href="https://github.com/disqus/sso-demo/tree/main/packages/backend" target="_blank" rel="noopener noreferrer">View Backend Source Code on GitHub</a>
+        <DisqusSSO />
     </div>
   );
 }
